c2416a0c3b5ae7f3f049630ffb2a269c985e3525
Vasilii Rogin <vasilii@giosg.com>
2020-06-08 10:52:47 +0300

    Add sample data

--- 10.ts
+++ 10.ts
@@ -29,25 +29,25 @@ function rDistribution(data: number[][]) {
   let Q = 0;
   data.forEach((arr) => arr.forEach((val) => (Q += val ** 2)));
   Q = Q - (A * A) / n;
-  log(`Q=${Q}`);
+  log(`Q=${Q.toFixed(2)}`);
 
   let Q1 = 0;
   Aj.forEach((ajVal, idx) => (Q1 += Aj[idx] ** 2 / data[idx].length));
   Q1 = Q1 - (A * A) / n;
-  log(`Q1 = ${Q1}`);
+  log(`Q1 = ${Q1.toFixed(2)}`);
 
   const Q2 = Q - Q1;
-  log(`Q2 = ${Q2}`);
+  log(`Q2 = ${Q2.toFixed(2)}`);
 
   const S12 = Q1 / (r - 1);
   const S22 = Q2 / (n - r);
-  log(`S₁²=${S12}`;
-  log(`S₂²=${S22}`);
+  log(`S₁²=${S12.toFixed(2)}`;
+  log(`S₂²=${S22.toFixed(2)}`);
   const Z = S12 / S22;
-  log(`Z = ${Z}`);
+  log(`Z = ${Z.toFixed(2)}`);
 
   const p = fisher(r-1, n-r, Z);
-  log(`p=${p}`)
+  log(`F(${r-1},${n-r},${Z.toFixed(2)})=${p.toFixed(4)}`)
 }
 
 const DIST_SIGMA = 4;
@@ -57,4 +57,8 @@ const mySelections = [50, 80, 70].map((n) =>
   getNormalSet(n).map((x) => Math.round(x * DIST_SIGMA + DIST_A))
 );
 
-rDistribution(mySelections);
+
+// https://www.matburo.ru/Examples/Files/ms_da_1.pdf
+const sampleData = [[18, 28, 12,14,32],[24,36,28,40,16],[36,12,22,45,40]]
+
+rDistribution(sampleData);
